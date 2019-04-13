4acd2c0de80f2980ded5e2fe740d54ee92f88476
JoseTyler101 <JoseTyler101@gmail.com>
2019-04-12 22:30:35 -0400

    Added Alphabet Array

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 function intro(){
     location.replace("../pages/intro.html")
 }
+
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
+                'm','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+
+
+function correctLetter(){
+    if(Answer)
+
+
+
+}
